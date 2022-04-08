c01a2c279dac0fd27da414889eb6d423a0ae578c
janosch314 <jan.harms@gssi.it>
2022-04-08 09:43:30 -0700

    sign flip in the complex phase of the lalsim waveforms (remember: it might be better to switch GWFish phase convention to match LALSim, not vice versa)

--- GWFish/modules/waveforms.py
+++ GWFish/modules/waveforms.py
@@ -143,9 +143,9 @@ def lal_caller(waveform, frequencyvector, mass_1, mass_2, luminosity_distance, r
     # ff_lal = np.arange(h_plus.data.length) * df
     i0 = int(round((f_min - h_plus.f0) / df))  # LAL starts from zero frequency!
     # Add initial 2pi*f*tc - phic - pi/4 to phase
-    phi_in = np.exp(1.j*(2*frequencyvector[:,0]*np.pi*geocent_time - phase - np.pi/4.))    
-    hp = np.conjugate(phi_in * h_plus.data.data[i0:i0 + len(frequencyvector)])  # it's already multiplied by the phase
-    hc = np.conjugate(phi_in * h_cross.data.data[i0:i0 + len(frequencyvector)])
+    phi_in = np.exp(1.j*(2*frequencyvector[:,0]*np.pi*geocent_time - phase - np.pi/4.))
+    hp = phi_in * np.conjugate(h_plus.data.data[i0:i0 + len(frequencyvector)])  # it's already multiplied by the phase
+    hc = phi_in * np.conjugate(h_cross.data.data[i0:i0 + len(frequencyvector)])
 
     hp = hp[:, np.newaxis]
     hc = hc[:, np.newaxis]
