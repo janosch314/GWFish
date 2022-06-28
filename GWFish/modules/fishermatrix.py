df857dfa1b1db18e26ec01a0e460fc9e0683288e
Jan Harms <jan.harms@gssi.it>
2022-06-28 10:56:28 +0200

    Make it possible to normalize Fisher matrices with a zero on the diagonal

--- GWFish/modules/fishermatrix.py
+++ GWFish/modules/fishermatrix.py
@@ -5,13 +5,16 @@ import GWFish.modules.auxiliary as aux
 
 def invertSVD(matrix):
     dm = np.sqrt(np.diag(matrix))
+    dm = np.maximum(dm, 1e-20*np.ones_like(dm))
     normalizer = np.outer(dm, dm)
     matrix_norm = matrix / normalizer
 
     [U, S, Vh] = np.linalg.svd(matrix_norm)
+
     thresh = 1e-10
     kVal = sum(S > thresh)
     matrix_inverse_norm = U[:, 0:kVal] @ np.diag(1. / S[0:kVal]) @ Vh[0:kVal, :]
+
     # print(matrix @ (matrix_inverse_norm / normalizer))
 
     return matrix_inverse_norm / normalizer
