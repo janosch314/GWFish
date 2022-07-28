ce357cf136cd79aa28db1d0c6b8b0005be1307c8
jacopok <jacopok@gmail.com>
2022-07-28 13:18:48 +0200

    fix: switched branch for github project reference
    this allows the edit button  to work
    since before it was pointing to "master"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,6 +59,12 @@ exclude_patterns = []
 #
 html_theme = 'furo'
 
+html_theme_options = {
+    "source_repository": "https://github.com/janosch314/GWFish",
+    "source_branch": "main",
+    "source_directory": "docs/",
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
