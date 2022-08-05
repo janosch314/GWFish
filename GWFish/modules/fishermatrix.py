31d83e851eefb3a0ac926026780dc285a0eab208
Jan Harms <jan.harms@gssi.it>
2022-08-05 09:47:59 +0200

    SVD modifications; probably obsolete at this point

--- GWFish/modules/fishermatrix.py
+++ GWFish/modules/fishermatrix.py
@@ -4,14 +4,14 @@ import GWFish.modules.detection as det
 import GWFish.modules.auxiliary as aux
 
 def invertSVD(matrix):
+    thresh = 1e-10
+
     dm = np.sqrt(np.diag(matrix))
-    dm = np.maximum(dm, 1e-20*np.ones_like(dm))
     normalizer = np.outer(dm, dm)
     matrix_norm = matrix / normalizer
 
     [U, S, Vh] = np.linalg.svd(matrix_norm)
 
-    thresh = 1e-10
     kVal = sum(S > thresh)
     matrix_inverse_norm = U[:, 0:kVal] @ np.diag(1. / S[0:kVal]) @ Vh[0:kVal, :]
 
