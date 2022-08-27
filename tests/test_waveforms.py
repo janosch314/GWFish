569abb53e2a5fa97925e2ae19ebdbfb34e7c20c8
jacopok <jacopok@gmail.com>
2022-08-27 18:50:38 +0200

    test: maximum-frequency parameter for waveforms

---
+++ tests/test_waveforms.py
@@ -0,0 +1,28 @@
+import numpy as np
+from GWFish.modules.waveforms import hphc_amplitudes
+from GWFish.modules.detection import Detector
+
+def test_max_f_cutoff():
+    
+    params = {
+        'mass_1': 1.4, 
+        'mass_2': 1.4, 
+        'redshift': 0.01,
+        'luminosity_distance': 40,
+        'theta_jn': 5/6 * np.pi,
+        'ra': 3.45,
+        'dec': -0.41,
+        'psi': 1.6,
+        'phase': 0,
+        'geocent_time': 1187008882, 
+        'max_frequency': 400,
+    }
+    
+    detector = Detector('ET', parameters = [None], fisher_parameters = [None])
+    
+    hphc, t_of_f = hphc_amplitudes('gwfish_TaylorF2', params, detector.frequencyvector)
+    
+    assert hphc[-1, 0] == 0j
+    assert hphc[-1, 1] == 0j
+    assert hphc[0, 0] != 0j
+    assert hphc[0, 1] != 0j
