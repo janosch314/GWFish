26fd14be8c921c40ca83f9dbde937249708bb82e
jacopok <jacopok@gmail.com>
2023-09-09 15:36:37 +0200

    new test for coordinates being solar-centered

--- tests/test_ephemeris.py
+++ tests/test_ephemeris.py
@@ -2,6 +2,8 @@ from GWFish.modules import ephemeris
 from time import perf_counter
 import numpy as np
 import matplotlib.pyplot as plt
+from astropy.coordinates import EarthLocation
+import pytest
 
 def time_execution(func, *args, **kwargs):
     t1 = perf_counter()
@@ -12,8 +14,8 @@ def time_execution(func, *args, **kwargs):
 def test_ephemeris_caching():
     moon = ephemeris.MoonEphemeris()
     
-    # about 3 years
-    times = np.linspace(0, 1e8, num=10_000)
+    # about 30 years
+    times = np.linspace(0, 1e9, num=10_000)
     
     first_time = time_execution(moon.get_coordinates, times)
     second_time = time_execution(moon.get_coordinates, times)
@@ -49,4 +51,56 @@ def test_earth_vs_moon(plot):
         plt.plot(times/(3600*24*30), r_y/384e6)
         plt.plot(times/(3600*24*30), r_z/384e6)
         
+        plt.xlabel('Time [months]')
+        plt.ylabel('Distance [normalized to avg Earth/Moon distance]')
         plt.show()
+        
+def test_earth_center_vs_location(plot):
+    
+    earth = ephemeris.EarthEphemeris()
+    loc = ephemeris.EarthLocationEphemeris(EarthLocation.of_site('V1'))
+    
+    times = np.linspace(0, 3600*24*2, num=10_000)
+    
+    x_loc, y_loc, z_loc = loc.get_coordinates(times)
+    x_earth, y_earth, z_earth = earth.get_coordinates(times)
+    
+    r_x, r_y, r_z = x_loc - x_earth, y_loc - y_earth, z_loc - z_earth
+    
+    r = np.sqrt(r_x**2 + r_y**2 + r_z**2)
+    
+    # first sanity check: Earth - surface distance is reasonable 
+    # within a large tolerance, since it varies from perigee to apogee
+    # breakpoint()
+    assert np.allclose(r, 6378e3, rtol=1e-2)
+    
+    if plot:
+        plt.plot(times/(3600*24), r_x/6378e3)
+        plt.plot(times/(3600*24), r_y/6378e3)
+        plt.plot(times/(3600*24), r_z/6378e3)
+        plt.plot(times/(3600*24), r/6378e3, label='Center of the Earth to location')
+        
+        plt.xlabel('Time [days]')
+        plt.ylabel('Distance [normalized to Earth radius]')
+        plt.show()
+        
+
+@pytest.mark.parametrize('ephem', [
+    ephemeris.EarthEphemeris(),
+    ephemeris.EarthLocationEphemeris(EarthLocation.of_site('V1')),
+    ephemeris.MoonEphemeris(),
+])
+def test_all_coordinates_are_solar_centered(ephem):
+    
+    # a day
+    times = np.linspace(0, 3600*24, num=10_000)
+    
+    x, y, z = ephem.get_coordinates(times)
+    
+    r = np.sqrt(x**2 + y**2 + z**2)
+    
+    
+    assert not np.isnan(r).any()
+    
+    # the radius should be roughly an astronomical unit
+    assert np.allclose(r, 150e9, rtol=5e-2)
