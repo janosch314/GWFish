01e9b97e155b8230ebe71ce3a63f1fc611e97c69
jacopok <jacopok@gmail.com>
2023-09-11 16:48:01 +0200

    new test for LGWA sky localization

---
+++ tests/test_localization.py
@@ -0,0 +1,41 @@
+from GWFish.modules.fishermatrix import compute_network_errors
+from GWFish.modules.detection import Network
+import pandas as pd
+import numpy as np
+
+
+def test_gw170817_localization():
+    
+    params = {
+        'mass_1': 1.4957673, 
+        'mass_2': 1.24276395, 
+        'redshift': 0.00980,
+        'luminosity_distance': 43.74755446,
+        'theta_jn': 2.545065595974997,
+        'ra': 3.4461599999999994,
+        'dec': -0.4080839999999999,
+        'psi': 0.,
+        'phase': 0.,
+        'geocent_time': 1187008882.4,
+        'a_1':0.005136138323169717, 
+        'a_2':0.003235146993487445, 
+        'lambda_1':368.17802383555687, 
+        'lambda_2':586.5487031450857,
+    }
+    df = pd.DataFrame.from_dict({
+        param: np.array([value])
+        for param, value in params.items()}
+    )
+    
+    network = Network(['LGWA'])
+    
+    network_snr, parameter_errors, sky_localization = compute_network_errors(
+        network,
+        df,
+        fisher_parameters=list(params.keys()),
+        waveform_model='TaylorF2'
+    )
+
+    skyloc_arcmin_square = 2 * sky_localization * (180/np.pi)**2 * (-np.log(1-0.9)) * 60**2
+    
+    assert np.isclose(skyloc_arcmin_square, 3, rtol=0.2)
