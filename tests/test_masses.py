3848a21adc0212e3be6893f34eea100e4955909e
u-dupletsa <ulyana.dupletsa@gssi.it>
2024-03-07 12:56:12 +0100

    cleaned df creation

--- tests/test_masses.py
+++ tests/test_masses.py
@@ -22,60 +22,40 @@ def test_snr_different_mass_inputs():
     network = Network(detector_ids = detectors, detection_SNR = (0., 8.))
     waveform_model = 'IMRPhenomD_NRTidalv2'
 
+    # Fixed parameters
     z = np.array([0.00980])
+    extra_params = {'redshift': z,
+        'luminosity_distance': Planck18.luminosity_distance(z).value,
+        'theta_jn': np.array([2.545065595974997]),
+        'ra': np.array([3.4461599999999994]),
+        'dec': np.array([-0.4080839999999999]),
+        'psi': np.array([0.]),
+        'phase': np.array([0.]),
+        'geocent_time': np.array([1187008882.4]),
+        'a_1':np.array([0.005136138323169717]), 
+        'a_2':np.array([0.003235146993487445]), 
+        'lambda_1':np.array([368.17802383555687]), 
+        'lambda_2':np.array([586.5487031450857])}
 
-    parameters_source = {
+
+    # Change mass parameters parametrization
+    masses_source = {
         'mass_1_source': np.array([1.4957673]), 
-        'mass_2_source': np.array([1.24276395]), 
-        'redshift': z,
-        'luminosity_distance': Planck18.luminosity_distance(z).value,
-        'theta_jn': np.array([2.545065595974997]),
-        'ra': np.array([3.4461599999999994]),
-        'dec': np.array([-0.4080839999999999]),
-        'psi': np.array([0.]),
-        'phase': np.array([0.]),
-        'geocent_time': np.array([1187008882.4]),
-        'a_1':np.array([0.005136138323169717]), 
-        'a_2':np.array([0.003235146993487445]), 
-        'lambda_1':np.array([368.17802383555687]), 
-        'lambda_2':np.array([586.5487031450857])}
-    parameters_source = pd.DataFrame(parameters_source)
+        'mass_2_source': np.array([1.24276395])}
 
-    parameters_det = {
+    masses_det = {
         'mass_1': np.array([1.4957673]) * (1 + z), 
-        'mass_2': np.array([1.24276395]) * (1 + z), 
-        'redshift': z,
-        'luminosity_distance': Planck18.luminosity_distance(z).value,
-        'theta_jn': np.array([2.545065595974997]),
-        'ra': np.array([3.4461599999999994]),
-        'dec': np.array([-0.4080839999999999]),
-        'psi': np.array([0.]),
-        'phase': np.array([0.]),
-        'geocent_time': np.array([1187008882.4]),
-        'a_1':np.array([0.005136138323169717]), 
-        'a_2':np.array([0.003235146993487445]), 
-        'lambda_1':np.array([368.17802383555687]), 
-        'lambda_2':np.array([586.5487031450857])}
-    parameters_det = pd.DataFrame(parameters_det)
+        'mass_2': np.array([1.24276395]) * (1 + z)}
 
 
     chirp_mass, mass_ratio = from_m1_m2_to_mChirp_q(1.4957673, 1.24276395)
-    parameters_chirp = {
+    masses_chirpq = {
         'chirp_mass': np.array([chirp_mass]) * (1 + z), 
-        'mass_ratio': np.array([mass_ratio]),
-        'redshift': z,
-        'luminosity_distance': Planck18.luminosity_distance(z).value,
-        'theta_jn': np.array([2.545065595974997]),
-        'ra': np.array([3.4461599999999994]),
-        'dec': np.array([-0.4080839999999999]),
-        'psi': np.array([0.]),
-        'phase': np.array([0.]),
-        'geocent_time': np.array([1187008882.4]),
-        'a_1':np.array([0.005136138323169717]), 
-        'a_2':np.array([0.003235146993487445]), 
-        'lambda_1':np.array([368.17802383555687]), 
-        'lambda_2':np.array([586.5487031450857])}
-    parameters_chirp = pd.DataFrame(parameters_chirp)
+        'mass_ratio': np.array([mass_ratio])}
+
+    parameters_source = pd.DataFrame(masses_source | extra_params)
+    parameters_det = pd.DataFrame(masses_det | extra_params)
+    parameters_chirp = pd.DataFrame(masses_chirpq | extra_params)
 
     _, snr_source, _, _ = compute_network_errors(
         network = network,
